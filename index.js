9e02974285c4a123541df420c8ae817cb6a3f59c
root <root@git.maharshi.ninja>
2021-12-04 14:18:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const tinykeys = require('tinykeys')
+
+exports.setupKeybindings = function setupKeybindings (el, bindings, ...extra) {
+  const newBindings = {}
+  for (const [k, f] of Object.entries(bindings)) {
+    newBindings[k] = (ev, ...args) => {
+      const active = ev.target
+      const enteringText = active instanceof HTMLElement &&
+        (active.isContentEditable ||
+          active.tagName === 'INPUT' ||
+          active.tagName === 'TEXTAREA' ||
+          active.tagName === 'SELECT'
+        )
+      if (!enteringText) return f(ev, ...args)
+    }
+  }
+
+  return tinykeys(el, newBindings, ...extra)
+}
